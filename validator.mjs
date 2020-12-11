9c1c2e393fc07225907bbf4bc5b6a9a658bcc255
nertc <davidtsiklauri7@gmail.com>
2020-12-11 15:23:57 +0400

    feat(Validator): Finish Validator

---
+++ validator.mjs
@@ -0,0 +1,22 @@
+export default class Validator {
+    static isTypeOf( val, type, error = `Value (${val}) is not type of ${type}` ) {
+        if( typeof val !== type ) {
+            throw new TypeError(error);
+        }
+    }
+
+    static isInstanceOf( val, type, error = `Value (${val}) is not instance of ${type}` ) {
+        if( !(val instanceof type) ) {
+            throw new TypeError(error);
+        }
+    }
+    
+    static compare( number1, number2, callback, error = `Numbers ${number1} and ${number2} don't satisfy function:\n${callback}` ) {
+        this.isTypeOf( number1, 'number' );
+        this.isTypeOf( number2, 'number' );
+        this.isTypeOf( callback, 'function' );
+        if( callback(number1, number2 ) ) {
+            throw new RangeError(error);
+        }
+    }
+}
