e0328aeaae9494c0735ca2a4f0aedb9b5e0d0199
nertc <davidtsiklauri7@gmail.com>
2020-12-12 16:47:34 +0400

    feat(Error): Finish print() function

--- error.mjs
+++ error.mjs
@@ -1,12 +1,20 @@
 import deepCopy from './copy.mjs';
 
 export default class CustomError extends Error {
+    #properties;
     constructor(message, properties) {
         super(message);
+        this.#properties = [];
         if( typeof properties === 'object' && properties !== null ) {
             Object.getOwnPropertyNames(properties).forEach( property => {
                 this[property] = deepCopy(properties[property]);
+                this.#properties.push(property);
             });
         }
+    }
+
+    print() {
+        console.log(this.stack);
+        this.#properties.forEach(prop => console.log(this[prop]));
     }
 }
