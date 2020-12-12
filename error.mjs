f19f65973218d025c1bb3a1444e915e1f25b6ffe
nertc <davidtsiklauri7@gmail.com>
2020-12-12 12:51:00 +0400

    feat(Error.toString): Finish CustomError.prototype.toString() function

--- error.mjs
+++ error.mjs
@@ -1,12 +1,23 @@
 import deepCopy from './copy.mjs';
 
 export default class CustomError extends Error {
+    #properties;
     constructor(message, properties) {
         super(message);
+        this.#properties = [];
         if( typeof properties === 'object' && properties !== null ) {
             Object.getOwnPropertyNames(properties).forEach( property => {
                 this[property] = deepCopy(properties[property]);
+                this.#properties.push(property);
             });
         }
+    }
+
+    toString() {
+        let output = `${this.name}: ${this.message}`;
+        output = this.#properties.reduce((prev, cur) => `${prev}\n${cur}`, output);
+        if( this.stack ) output += `\n${this.stack}`;
+        
+        return output;
     }
 }
